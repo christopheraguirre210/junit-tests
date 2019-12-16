029e93f4c5d97f7301eb4dc83f9509587314ce05
Christopher Aguirre <christopher.aguirre210@gmail.com>
2019-12-16 11:32:24 -0600

    constructor and getters made in the Student class

--- src/Student.java
+++ src/Student.java
@@ -6,4 +6,25 @@ public class Student {
     String name;
     ArrayList<Integer> grades;
 
+    public Student(long id, String name, ArrayList<Integer> grades) {
+        this.id = id;
+        this.name = name;
+        this.grades = grades;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void addGrade(int grade){
+
+    }
+
+    public double getGradeAverage(){
+        return getGradeAverage();
+    }
 }
